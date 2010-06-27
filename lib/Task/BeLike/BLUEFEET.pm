27a91a1bb9afe9c1983c46cf07948c9aad2dbb84
Aran Clary Deltac <bluefeet@cpan.org>
2010-06-27 00:33:43 +0000

    Import of BLUEFEET/Task-BeLike-BLUEFEET-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-BLUEFEET
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BLUEFEET/Task-BeLike-BLUEFEET-0.01.tar.gz
    gitpan-cpan-author:       BLUEFEET
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/BLUEFEET.pm
@@ -0,0 +1,17 @@
+package Task::BeLike::BLUEFEET;
+BEGIN {
+  $Task::BeLike::BLUEFEET::VERSION = '0.01';
+}
+1;
+__END__
+
+=head1 NAME
+
+Task::BeLike::BLUEFEET - Get your feet died blue (metaphorically of course).
+
+=head1 DESCRIPTION
+
+This distrubition does nothing on its own, instead it causes the installation
+of dozens of other modules.  These modules are the modules that BLUEFEET
+thinks are good and useful.
+
